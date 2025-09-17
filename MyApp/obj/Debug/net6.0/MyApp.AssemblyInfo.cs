95c0853e76d1e9c48285f283aa2146deb5752b8c
Shengyw1711 <syw783153@gmail.com>
2025-09-17 19:24:44 +0800

    product accessory auto load finished

--- MyApp/obj/Debug/net6.0/MyApp.AssemblyInfo.cs
+++ MyApp/obj/Debug/net6.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69fb4b85f178b77707f1bff02426264ff342371c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9653f0591e83a2200f29e315eb25fce177b829dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
